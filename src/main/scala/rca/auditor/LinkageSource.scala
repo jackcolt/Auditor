ad45a54c61e5bd206dc03f878b941b3b4d3a30e9
jackcolt <jack.colt@gmail.com>
2018-11-04 14:26:27 -0500

    added/modified constructors

--- src/main/scala/rca/auditor/LinkageSource.scala
+++ src/main/scala/rca/auditor/LinkageSource.scala
@@ -5,7 +5,7 @@ import scala.beans.BeanProperty
 class LinkageSource (var name:String) extends Serializable {
 
 
-  @BeanProperty val population: Int = _
-  @BeanProperty val eligiblePopulation: Int = _
+  @BeanProperty var population: Int = _
+  @BeanProperty var eligiblePopulation: Int = _
 
 }
