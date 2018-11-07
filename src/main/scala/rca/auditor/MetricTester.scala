4c0df9b68d5055f6f17cc2ccf406cab260c8c771
jackcolt <jack.colt@gmail.com>
2018-11-07 15:45:24 -0500

    implemented the first pass of a regression model to test the linkage

--- src/main/scala/rca/auditor/MetricTester.scala
+++ src/main/scala/rca/auditor/MetricTester.scala
@@ -1,6 +1,8 @@
 package rca.auditor
 
 import org.apache.kudu.spark.kudu._
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 import org.elasticsearch.spark.rdd.EsSpark
@@ -63,11 +65,6 @@ class MetricTester extends FunSuite {
 
     val kuduContext = new KuduContext(kuduMaster, sc)
 
-    //val masterAssessorDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "audit.master_assessor")).kudu
-    //val masterAssessorGeoIdDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "dev.master_assessor_geo_id")).kudu
-    //val masterTransactionsDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "audit.master_transactions")).kudu
-    //val deedDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "test.corelogic_deed")).kudu
-    //val parcelDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "test.corelogic_parcel")).kudu
 
     val analysisDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "impala::audit.dq_cl_analysis_test")).kudu
 
@@ -80,10 +77,10 @@ class MetricTester extends FunSuite {
 
     println ("metrics calculated on: "+metric.timeStamp+ " in time zone: "+metric.timeZone)
 
-    //metric.characteristics = new LinkageCharacteristic("apn_lev_ratio_score",0) :: metric.characteristics
-    //metric.characteristics = new LinkageCharacteristic("addr_lev_ratio_score",0) :: metric.characteristics
-    //metric.characteristics = new LinkageCharacteristic("buyer_lev_ratio_score",0) :: metric.characteristics
-    //metric.characteristics = new LinkageCharacteristic("seller_lev_ratio_score",0) :: metric.characteristics
+    metric.characteristics = new LinkageCharacteristic("apn_lev_ratio_score",0) :: metric.characteristics
+    metric.characteristics = new LinkageCharacteristic("addr_lev_ratio_score",0) :: metric.characteristics
+    metric.characteristics = new LinkageCharacteristic("buyer_lev_ratio_score",0) :: metric.characteristics
+    metric.characteristics = new LinkageCharacteristic("seller_lev_ratio_score",0) :: metric.characteristics
 
 
 
@@ -113,7 +110,63 @@ class MetricTester extends FunSuite {
     val rdd = spark.sparkContext.makeRDD(Seq(metric))
 
     EsSpark.saveToEs(rdd, "linkage_metrics/docs")
+
+
   }
 
+  test ("Regression Test") {
+
+    //val spark = SparkSession.builder().master("spark://172.31.30.117:7077").appName("Linkage Metrics").getOrCreate()
+
+    val spark = SparkSession.builder().master("local").appName("Linkage Metrics").getOrCreate()
+    //val conf = new SparkConf().setAppName("Linkage_DQ_VS_CL").setMaster("spark://172.31.30.117:7077")
+
+    spark.conf.set("spark.driver.allowMultipleContexts", "true")
+
+    val sc = spark.sparkContext
+
+
+    val sqlContext = spark.sqlContext
+
+    val kuduMaster = "172.31.37.251:7051"
+
+    val kuduContext = new KuduContext(kuduMaster, sc)
+
+
+    val analysisDF = sqlContext.read.options(Map("kudu.master" -> "172.31.37.251:7051", "kudu.table" -> "impala::audit.dq_cl_analysis_test")).kudu
+
+    analysisDF.createOrReplaceTempView("analysis")
+
+
+    val data = spark.sql(
+      "SELECT (1.0 - apn_lev_ratio_score) as label, addr_lev_ratio_score, buyer_lev_ratio_score from analysis " +
+    "limit 1000")
+
+
+
+    val trainingData = data.rdd.map(row =>
+      new LabeledPoint (
+        BigDecimal(row.getAs[Double](0)).setScale(0, BigDecimal.RoundingMode.FLOOR).toDouble,
+        Vectors.dense(row.getAs[Double](row.length-1))
+      )).cache()
+
+    // Building the model
+    val numIterations = 100
+    val stepSize = 0.00000001
+
+
+    val model = LinearRegressionWithSGD.train(trainingData, numIterations, stepSize)
+
+
+    // Evaluate model on training examples and compute training error
+    val valuesAndPreds = trainingData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+
+    val MSE = valuesAndPreds.map{ case(v, p) => math.pow((v - p), 2) }.mean()
+    println(s"training Mean Squared Error $MSE")
+
+  }
 }
 
